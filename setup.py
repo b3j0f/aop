d3a5e1a53330fa3a81d1a026bf80fd3b8e92b3c1
mrbozzo <jlabejof@yahoo.fr>
2014-10-16 09:42:31 +0200

    :remove: manual installation from travis config

--- setup.py
+++ setup.py
@@ -5,24 +5,18 @@ from setuptools import setup, find_packages
 
 from os.path import abspath, dirname, join
 
-from sys import path
-
 # get setup directory abspath
 _path = dirname(abspath(__file__))
-
-# import aspects
-path.append(_path)
-import b3j0f.aop as package
 
 # get long description
 with open(join(_path, 'README')) as f:
     desc = f.read()
 
-dependencies = ['b3j0f.utils==0.5.0']
+dependencies = ['b3j0f.utils==0.5.1']
 
 setup(
-    name=package.__name__,
-    version=package.__version__,
+    name='b3j0f.aop',
+    version='0.4.0',
     install_requires=dependencies,
     packages=find_packages(where='.', exclude=['test.*', '*.test.*']),
     author="b3j0f",
